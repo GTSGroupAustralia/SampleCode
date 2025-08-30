0e656147fc190a3f31d3bd768e97bf17b827afe1
John Messinger <john.messinger@gmail.com>
2025-08-30 17:41:51 +1000

    Python example for PI in C# - Initial commit

---
+++ GSharp.PythonRegressionAnalysis/GSharp.Testbed/GlobalDataProvider.cs
@@ -0,0 +1,22 @@
+﻿using GHost.GSharp.Core;
+
+namespace GSharp.Testbed
+{
+    public class GlobalDataProvider : GlobalDataProviderBase
+    {
+        public static GlobalDataProvider Instance = new GlobalDataProvider();
+
+        public static void Initialize() => GlobalData.GlobalDataProvider = Instance;
+
+
+        public override string ProductTitle => "gSharp Developer Testbed";
+
+        public override string ProductVersionString => Versions.ProductVersion;
+
+        public override string LogSource => "$DevTestbed";
+
+        public override string ProductName => "GHost.GSharp.Developer.Testbed";
+
+        public override string CompanyDataFolder => SolutionConstants.CompanyDataFolder;
+    }
+}
