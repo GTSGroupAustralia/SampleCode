fc4a104f446601fda3c3f0b9c246bba141acb2dc
John Messinger <john.messinger@gmail.com>
2025-09-09 14:20:26 +1000

    Add files via upload

---
+++ Samples/PISystem/CSharp/GSharp.PythonRegressionAnalysis/GSharp.Testbed/Program.Init.cs
@@ -0,0 +1,42 @@
+﻿using GHost.Core.Logging;
+using GHost.GSharp.Developer;
+using System;
+using System.Diagnostics;
+
+namespace GSharp.Testbed
+{
+    internal partial class Program
+    {
+        private void RunDebuggingHost()
+        {
+            ILogger log = null;
+
+            try
+            {
+                GlobalDataProvider.Initialize();
+
+                var host = new AssemblyTestHost();
+
+                ConfigureAssembly(host.GetBuilder());
+                ConfigureSettings(host.Settings);
+
+                log = host.Log;
+
+                host.Start();
+            }
+            catch (Exception ex)
+            {
+                if (log is null)
+                    Console.WriteLine(ex.ToString());
+                else
+                    log.Error(ex);
+            }
+
+            if (Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                _ = Console.ReadKey(intercept: true);
+            }
+        }
+    }
+}
---
+++ Samples/PISystem/CSharp/GSharp.PythonRegressionAnalysis/GSharp.Testbed/Program.cs
@@ -0,0 +1,32 @@
+﻿using GHost.Core.Logging;
+using GHost.GSharp.Core.Configuration.Logging;
+using GHost.GSharp.Developer.Builder;
+using GSharp.PythonRegressionAnalysis;
+using static GHost.GSharp.Developer.AssemblyTestHost;
+
+namespace GSharp.Testbed
+{
+    internal partial class Program
+    {
+        private static void Main()
+        {
+            new Program().RunDebuggingHost();
+        }
+
+        private void ConfigureAssembly(AssemblyBuilder builder)
+        {
+            builder.AddModule<LinearRegression>(moduleBuilder => moduleBuilder
+                .ConfigurePoint(nameof(LinearRegression.ProcessFeedrate), "|Process Feedrate")
+                .ConfigurePoint(nameof(LinearRegression.Intercept), "|Linear Regression Analysis|Intercept")
+                .ConfigurePoint(nameof(LinearRegression.Slope), "|Linear Regression Analysis|Intercept")
+                .AddContext("ContextName1", @"\\SALES-AF1\NuGreen\NuGreen\Tucson\Distilling Process\Equipment\P-871")
+                );
+        }
+
+        private void ConfigureSettings(TestHostSettings settings)
+        {
+            LogLevelsSettings logLevelSettings = settings.Logging.Levels;
+            logLevelSettings.ApplyToAll(LogLevel.Info);
+        }
+    }
+}
