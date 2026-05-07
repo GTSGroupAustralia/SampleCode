5f2c546d00576759a7da0b9a8ebd896cd531ef91
John Messinger <john.messinger@gtsgroup.com.au>
2026-05-07 21:47:09 +1000

    Updated project for gSharp 1.3.4 using new Visual Studio templates.

--- Samples/PISystem/CSharp/GSharp.PythonRegressionAnalysis/GSharp.Testbed/Program.cs
+++ Samples/PISystem/CSharp/GSharp.PythonRegressionAnalysis/GSharp.Testbed/Program.cs
@@ -1,32 +1,28 @@
-﻿using GHost.Core.Logging;
+using GHost.Core.Logging;
 using GHost.GSharp.Core.Configuration.Logging;
+using GHost.GSharp.Developer;
 using GHost.GSharp.Developer.Builder;
-using GSharp.PythonRegressionAnalysis;
-using static GHost.GSharp.Developer.AssemblyTestHost;
 
-namespace GSharp.Testbed
+namespace GSharp.Testbed;
+
+internal partial class Program
 {
-    internal partial class Program
+    private static void Main()
     {
-        private static void Main()
-        {
-            new Program().RunDebuggingHost();
-        }
+        RunTestbed();
+    }
 
-        private void ConfigureAssembly(AssemblyBuilder builder)
-        {
-            builder.AddModule<LinearRegression>(moduleBuilder => moduleBuilder
-                .ConfigurePoint(nameof(LinearRegression.ProcessFeedrate), "|Process Feedrate")
-                .ConfigurePoint(nameof(LinearRegression.Intercept), "|Linear Regression Analysis|Intercept")
-                .ConfigurePoint(nameof(LinearRegression.Slope), "|Linear Regression Analysis|Intercept")
-                .AddContext("ContextName1", @"\\SALES-AF1\NuGreen\NuGreen\Tucson\Distilling Process\Equipment\P-871")
-                );
-        }
-
-        private void ConfigureSettings(TestHostSettings settings)
-        {
-            LogLevelsSettings logLevelSettings = settings.Logging.Levels;
-            logLevelSettings.ApplyToAll(LogLevel.Info);
-        }
+    private void ConfigureAssembly(AssemblyBuilder builder)
+    {
+        builder
+            .AddModule<GSharp.PythonRegressionAnalysis.LinearRegression>(moduleBuilder => moduleBuilder
+                .ConfigurePoint(nameof(GSharp.PythonRegressionAnalysis.LinearRegression.ProcessFeedrate), "Process Feedrate")
+                .ConfigurePoint(nameof(GSharp.PythonRegressionAnalysis.LinearRegression.Intercept), "Linear Regression Analysis|Intercept")
+                .ConfigurePoint(nameof(GSharp.PythonRegressionAnalysis.LinearRegression.Slope), "Linear Regression Analysis|Slope")
+                .AddContext($"{nameof(GSharp.PythonRegressionAnalysis.LinearRegression)} Test Context", @"\\DEV-AF1\NuGreen\NuGreen\Tucson\Distilling Process\Equipment\P-871")
+            )
+            .RunAsAt("*")
+            .WithMinimumAssemblyLogLevel(LogLevel.Info)
+            ;
     }
 }
